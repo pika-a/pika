1b9f9c4b9775a084c2fa527c5a0aa8d8d65424cc
pika-a <52085276+pika-a@users.noreply.github.com>
2019-06-23 01:10:33 +0300

    Update pika.lua

--- pika.lua
+++ pika.lua
@@ -94,7 +94,7 @@ echo -e ""
 done
 ]])  
 file:close()  
-file = io.open("pi", "w")  
+file = io.open("PI", "w")  
 file:write([[
 killall screen
 while(true) do
@@ -105,7 +105,7 @@ echo -e "pika IS RUN BOT"
 ]])  
 file:close() 
 os.execute('rm -fr $HOME/.telegram-cli')
-os.execute('./pi')
+os.execute('./PI')
  end 
 local serialize_to_file = function(data, file, uglify)  
 file = io.open(file, "w+")  
